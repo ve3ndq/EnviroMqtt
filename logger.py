35aabd6237972889ed2811e91867b0b9d763fba7
ve3ndq <ngjdykstra@gmail.com>
2021-12-26 10:57:33 -0500

    Initial Commit

---
+++ logger.py
@@ -0,0 +1,129 @@
+import collections, threading, traceback
+
+import paho.mqtt.client as mqtt
+
+try:
+    # Transitional fix for breaking change in LTR559
+    from ltr559 import LTR559
+    ltr559 = LTR559()
+except ImportError:
+    import ltr559
+
+from bme280 import BME280
+from pms5003 import PMS5003
+from enviroplus import gas
+
+
+class EnvLogger:
+    def __init__(self, client_id, host, port, username, password, prefix, use_pms5003, num_samples):
+        self.bme280 = BME280()
+
+        self.prefix = prefix
+
+        self.connection_error = None
+        self.client = mqtt.Client(client_id=client_id)
+        self.client.on_connect = self.__on_connect
+        self.client.username_pw_set(username, password)
+        self.client.connect(host, port)
+        self.client.loop_start()
+
+        self.samples = collections.deque(maxlen=num_samples)
+        self.latest_pms_readings = {}
+
+        if use_pms5003:
+            self.pm_thread = threading.Thread(target=self.__read_pms_continuously)
+            self.pm_thread.daemon = True
+            self.pm_thread.start()
+    
+
+    def __on_connect(self, client, userdata, flags, rc):
+        errors = {
+            1: "incorrect MQTT protocol version",
+            2: "invalid MQTT client identifier",
+            3: "server unavailable",
+            4: "bad username or password",
+            5: "connection refused"
+        }
+
+        if rc > 0:
+            self.connection_error = errors.get(rc, "unknown error")
+
+
+    def __read_pms_continuously(self):
+        """Continuously reads from the PMS5003 sensor and stores the most recent values
+        in `self.latest_pms_readings` as they become available.
+
+        If the sensor is not polled continously then readings are buffered on the PMS5003,
+        and over time a significant delay is introduced between changes in PM levels and 
+        the corresponding change in reported levels."""
+
+        pms = PMS5003()
+        while True:
+            try:
+                pm_data = pms.read()
+                #print (pm_data)
+
+#                pm100 = pm_data.pm_per_1l_air(10.0)
+#                pm50  = pm_data.pm_per_1l_air(5.0) - pm100
+#                pm25  = pm_data.pm_per_1l_air(2.5) - pm100 - pm50
+#                pm10  = pm_data.pm_per_1l_air(1.0) - pm100 - pm50 - pm25
+#                pm5   = pm_data.pm_per_1l_air(0.5) - pm100 - pm50 - pm25 - pm10
+#                pm3   = pm_data.pm_per_1l_air(0.3) - pm100 - pm50 - pm25 - pm10 - pm5
+
+                self.latest_pms_readings = {
+                    "particulate/1.0": pm_data.pm_ug_per_m3(1.0, atmospheric_environment=True),
+                    "particulate/2.5": pm_data.pm_ug_per_m3(2.5, atmospheric_environment=True),
+                    "particulate/10.0": pm_data.pm_ug_per_m3(None, atmospheric_environment=True),
+ #                   "particulate/pm100": pm100,
+ #                   "particulate/pm50": pm50,
+ #                   "particulate/pm25": pm25,
+ #                   "particulate/pm10": pm10,
+ #                   "particulate/pm5": pm5,
+ #                   "particulate/pm3": pm3,
+
+
+
+
+                }
+            except:
+                print("Failed to read from PMS5003. Resetting sensor.")
+                traceback.print_exc()
+                pms.reset()
+
+
+    def take_readings(self):
+        gas_data = gas.read_all()
+        readings = {
+            "proximity": ltr559.get_proximity(),
+            "lux": ltr559.get_lux(),
+            "temperature": self.bme280.get_temperature(),
+            "pressure": self.bme280.get_pressure(),
+            "humidity": self.bme280.get_humidity(),
+            "gas/oxidising": gas_data.oxidising,
+            "gas/reducing": gas_data.reducing,
+            "gas/nh3": gas_data.nh3,
+        }
+
+        readings.update(self.latest_pms_readings)
+        
+        return readings
+
+
+    def publish(self, topic, value):
+        topic = self.prefix.strip("/") + "/" + topic
+        self.client.publish(topic, str(value))
+#        self.client.publish("ABCCCC","123")
+
+    def update(self, publish_readings=True):
+        self.samples.append(self.take_readings())
+
+        if publish_readings:
+            for topic in self.samples[0].keys():
+                value_sum = sum([d[topic] for d in self.samples])
+                value_avg = value_sum / len(self.samples)
+                self.publish(topic, value_avg)
+
+
+    def destroy(self):
+        self.client.disconnect()
+        self.client.loop_stop()
---
+++ main.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+
+import argparse, time, sys
+
+from logger import EnvLogger
+
+#main.py -h 192.168.5.200  --use-pms5003
+
+def parse_args():
+    ap = argparse.ArgumentParser(add_help=False)
+    ap.add_argument("-h", "--host", required=False,default="192.168.5.200", help="the MQTT host to connect to")
+    ap.add_argument("-p", "--port", type=int, default=1883, help="the port on the MQTT host to connect to")
+    ap.add_argument("-U", "--username", default=None, help="the MQTT username to connect with")
+    ap.add_argument("-P", "--password", default=None, help="the password to connect with")
+    ap.add_argument("--prefix", default="ENVIRO", help="the topic prefix to use when publishing readings, i.e. 'lounge/enviroplus'")
+    ap.add_argument("--client-id", default="", help="the MQTT client identifier to use when connecting")
+    ap.add_argument("--interval", type=int, default=5, help="the duration in seconds between updates")
+    ap.add_argument("--delay", type=int, default=5, help="the duration in seconds to allow the sensors to stabilise before starting to publish readings")
+    ap.add_argument("--use-pms5003", default=True, action="store_true", help="if set, PM readings will be taken from the PMS5003 sensor")
+    ap.add_argument("--help", action="help", help="print this help message and exit")
+    return vars(ap.parse_args())
+
+
+def main():
+    args = parse_args()
+
+    # Initialise the logger
+    logger = EnvLogger(
+        client_id=args["client_id"],
+        host=args["host"],
+        port=args["port"],
+        username=args["username"],
+        password=args["password"],
+        prefix=args["prefix"],
+        use_pms5003=args["use_pms5003"],
+        num_samples=args["interval"]
+    )
+
+    # Take readings without publishing them for the specified delay period,
+    # to allow the sensors time to warm up and stabilise
+    publish_start_time = time.time() + args["delay"]
+    while time.time() < publish_start_time:
+        logger.update(publish_readings=False)
+        time.sleep(1)
+
+    # Start taking readings and publishing them at the specified interval
+    next_sample_time = time.time()
+    next_publish_time = time.time() + args["interval"]
+
+    while True:
+        if logger.connection_error is not None:
+            sys.exit(f"Connecting to the MQTT server failed: {logger.connection_error}")
+        
+        should_publish = time.time() >= next_publish_time
+        if should_publish:
+            next_publish_time += args["interval"]
+        
+        logger.update(publish_readings=should_publish)
+
+        next_sample_time += 1
+        sleep_duration = max(next_sample_time - time.time(), 0)
+        time.sleep(sleep_duration)
+    
+    logger.destroy()
+
+
+if __name__ == "__main__":
+    main()
